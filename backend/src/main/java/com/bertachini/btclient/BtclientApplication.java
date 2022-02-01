14c62f728cc0c984fd1542edc9ae037c56fdf41a
ThiagoBertachini <thiago.bertachini@hotmail.com>
2022-02-01 18:20:23 -0300

    Backend project created

---
+++ backend/src/main/java/com/bertachini/btclient/BtclientApplication.java
@@ -0,0 +1,13 @@
+package com.bertachini.btclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bertachini/btclient/BtclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertachini.btclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
